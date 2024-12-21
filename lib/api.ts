ded537934dfec7d7c37b45d279594f99753d7ab9
AlexandrPitsak <alexandr.pitsak@gmail.com>
2024-12-21 22:36:04 +0200

    Add prettier Add prettier rules Add prettierignore file Prettify project by rules

--- lib/api.ts
+++ lib/api.ts
@@ -1,9 +1,9 @@
 import axios, { AxiosInstance } from 'axios';
-import playwrightConfig from "../playwright.config";
+import playwrightConfig from '../playwright.config';
 import { randomUUID } from 'crypto';
 
 if (!playwrightConfig?.use?.baseURL) {
-    throw new Error("Base URL is not defined in Playwright config.");
+    throw new Error('Base URL is not defined in Playwright config.');
 }
 
 export const axiosInstance: AxiosInstance = axios.create({
@@ -11,53 +11,58 @@ export const axiosInstance: AxiosInstance = axios.create({
     validateStatus: () => true,
 });
 
-axiosInstance.interceptors.request.use(function(config) {
-    const separator = '#'.repeat(50);
-    const requestId = randomUUID();
-    const requestStartTime = new Date();
+axiosInstance.interceptors.request.use(
+    function (config) {
+        const separator = '#'.repeat(50);
+        const requestId = randomUUID();
+        const requestStartTime = new Date();
 
-    config.headers['RequestUUID'] = requestId;
-    config.headers['RequestStartTime'] = requestStartTime.valueOf().toString();
+        config.headers['RequestUUID'] = requestId;
+        config.headers['RequestStartTime'] = requestStartTime.valueOf().toString();
 
-    const logData = {
-        requestId,
-        requestStartTime: requestStartTime.toLocaleTimeString(),
-        url: `${config.baseURL}${config.url}`,
-        method: config.method.toUpperCase(),
-        data: config.data,
-    };
+        const logData = {
+            requestId,
+            requestStartTime: requestStartTime.toLocaleTimeString(),
+            url: `${config.baseURL}${config.url}`,
+            method: config.method.toUpperCase(),
+            data: config.data,
+        };
 
-    const message = [separator, 'REQUEST:', JSON.stringify(logData, null, 2), separator].join('\n');
-    console.log(message);
+        const message = [separator, 'REQUEST:', JSON.stringify(logData, null, 2), separator].join('\n');
+        console.log(message);
 
-    return config;
-}, function(error) {
-    console.error(error);
+        return config;
+    },
+    function (error) {
+        console.error(error);
 
-    return Promise.reject(error);
-});
+        return Promise.reject(error);
+    }
+);
 
+axiosInstance.interceptors.response.use(
+    function (response) {
+        const separator = '#'.repeat(50);
+        const requestId = response.config.headers!['RequestUUID'];
+        const requestStartTime = Number(response.config.headers!['RequestStartTime']);
+        const elapsedTime = (Date.now() - requestStartTime) / 1000;
 
-axiosInstance.interceptors.response.use(function(response) {
-    const separator = '#'.repeat(50);
-    const requestId = response.config.headers!['RequestUUID'];
-    const requestStartTime = Number(response.config.headers!['RequestStartTime']);
-    const elapsedTime = (Date.now() - requestStartTime) / 1000;
+        const logData = {
+            status: response.status,
+            statusText: response.statusText,
+            requestId,
+            elapsedTime: `${elapsedTime} sec`,
+            responseData: response.data,
+        };
 
-    const logData = {
-        status: response.status,
-        statusText: response.statusText,
-        requestId,
-        elapsedTime: `${elapsedTime} sec`,
-        responseData: response.data,
-    };
+        const message = [separator, 'RESPONSE:', JSON.stringify(logData, null, 2), separator].join('\n');
+        console.log(message);
 
-    const message = [separator, 'RESPONSE:', JSON.stringify(logData, null, 2), separator].join('\n');
-    console.log(message);
+        return response;
+    },
+    function (error) {
+        console.error(error);
 
-    return response;
-}, function(error) {
-    console.error(error);
-
-    return Promise.reject(error);
-});
+        return Promise.reject(error);
+    }
+);
